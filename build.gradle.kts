a24d23bea4d764ff877a0d42b9a580db34e67308
VladDer27 <cadoni827@gmail.com>
2025-03-06 14:19:40 +0300

    Added kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
+	implementation("org.springframework.kafka:spring-kafka")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
 	compileOnly("org.projectlombok:lombok")
@@ -47,6 +48,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
 	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- src/main/java/com/vkr/tournament_service/config/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/vkr/tournament_service/config/kafka/KafkaConsumerConfig.java
@@ -1,7 +1,13 @@
 package com.vkr.tournament_service.config.kafka;
 
+import com.vkr.tournament_service.entity.match.Match;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @Configuration
 public class KafkaConsumerConfig {
+    @KafkaListener(topics = "match-finished-topic", groupId = "tournament-group")
+    public void consume(Match event) {
+        System.out.println("Получено событие о матче: " + event);
+    }
 }
