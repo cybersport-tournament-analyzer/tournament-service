40adc3e6c0737d318fa345810b2903facbdaa3d3
yofujitsu <yofujitsuuu@gmail.com>
2025-04-13 23:59:35 +0300

    match end event

--- src/main/java/com/vkr/tournament_service/kafka/event/matchEnd/MatchEndEvent.java
+++ src/main/java/com/vkr/tournament_service/kafka/event/matchEnd/MatchEndEvent.java
@@ -1,7 +1,6 @@
 package com.vkr.tournament_service.kafka.event.matchEnd;
 
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.vkr.tournament_service.dto.matchmaking.Match;
 import com.vkr.tournament_service.kafka.event.KafkaEvent;
 import lombok.AllArgsConstructor;
